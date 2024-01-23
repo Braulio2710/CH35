ba4702ef94f2854b938f097485a85680e05c9761
Braulio2710 <braulio.generation@gmail.com>
2024-01-22 23:05:54 -0600

    Se agregan comentarios para legibilidad de código.

--- Java Script/Repos de correción de código Dany 22-ene-24/codigo-otros-1/assets/js/app.js
+++ Java Script/Repos de correción de código Dany 22-ene-24/codigo-otros-1/assets/js/app.js
@@ -1,9 +1,11 @@
+//Definición de variables
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('.name'); //faltaba el punto antes de name para referenciar el id .name de la API
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
+//Definición de función asíncrona para mostrar texto "cargando mientras se accede a la API"
 async function displayUser(username) {//Se agrega asyn antes de function marca un error en el await de linea 9. Se tiene que gregar la palabra reservada async antes de la función en la linea 7, debido a que las funciones await solo se permiten en funciones asíncronas. 
   $n.textContent = 'cargando...';
   const data = await fetch(`${usersEndpoint}/${username}`); //ADEMAS se cambia la const "response" por "data"
